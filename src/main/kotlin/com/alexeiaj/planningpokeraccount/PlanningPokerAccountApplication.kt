3deae3c5e5dc82386d174d7b6bd97cd0d5d4c3ea
AlexeiJunior <alexei.junior@hotmail.com>
2024-01-11 23:45:06 -0300

    First commit

---
+++ src/main/kotlin/com/alexeiaj/planningpokeraccount/PlanningPokerAccountApplication.kt
@@ -0,0 +1,11 @@
+package com.alexeiaj.planningpokeraccount
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanningPokerAccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanningPokerAccountApplication>(*args)
+}
---
+++ src/test/kotlin/com/alexeiaj/planningpokeraccount/PlanningPokerAccountApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alexeiaj.planningpokeraccount
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanningPokerAccountApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
